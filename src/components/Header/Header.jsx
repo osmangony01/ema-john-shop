7f2df50383fd67569d07f8deb3e20019f16ad589
osmangony01 <mdosmangoni376@gmail.com>
2023-03-29 12:54:32 +0600

    make header section

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <div>
+                <img src="" alt="" />
+            </div>
+            <div>
+                <a href="/order">Order</a>
+                <a href="/review">Order Review</a>
+                <a href="/inventory">Manage Inventory</a>
+                <a href="/login">Login</a>
+            </div>
+        </div>
+    );
+};
+
+export default Header;<div>
+<img src="" alt="" /></div>
